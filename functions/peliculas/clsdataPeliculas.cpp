efd57fabe159fef1951fb6252592ff61641e1674
AYRP97 <405ayr@gmail.com>
2025-10-13 15:40:04 -0300

    Se añadio la clase dataPeliculas .cpp y .h

---
+++ functions/peliculas/clsdataPeliculas.cpp
@@ -0,0 +1,85 @@
+#include "clsdataPeliculas.h"
+#include <cstring>
+
+// Constructor por defecto
+clsDataPeliculas::clsDataPeliculas() {
+    std::strcpy(id, "");
+    std::strcpy(nombre, "");
+    duracion = 0;
+    std::strcpy(genero, "");
+    std::strcpy(clasificacion, "");
+    std::strcpy(director, "");
+    idiomas = "";
+    formatos = "";
+    activa = false;
+}
+
+
+
+
+// Getters
+std::string clsDataPeliculas::getId() const { 
+    return std::string(id); 
+}
+std::string clsDataPeliculas::getNombre() const { 
+    return std::string(nombre); 
+}
+int clsDataPeliculas::getDuracion() const { 
+    return duracion; 
+}
+std::string clsDataPeliculas::getGenero() const { 
+    return std::string(genero); 
+}
+std::string clsDataPeliculas::getClasificacion() const { 
+    return std::string(clasificacion); 
+}
+std::string clsDataPeliculas::getDirector() const { 
+    return std::string(director); 
+}
+/* void clsDataPeliculas::setFechaEstreno(const Fecha& fecha) { 
+ futura implementación 
+} */
+std::string clsDataPeliculas::getIdiomas() const { 
+    return idiomas; 
+}
+std::string clsDataPeliculas::getFormatos() const { 
+    return formatos; 
+}
+bool clsDataPeliculas::estaActiva() const { 
+    return activa; 
+}
+
+
+
+
+// Setters
+void clsDataPeliculas::setId(const std::string& nuevoId) { 
+    std::strncpy(id, nuevoId.c_str(), 10); id[10] = '\0'; 
+}
+void clsDataPeliculas::setNombre(const std::string& nuevoNombre) { 
+    std::strncpy(nombre, nuevoNombre.c_str(), 49); nombre[49] = '\0'; 
+}
+void clsDataPeliculas::setDuracion(int nuevaDuracion) { 
+    duracion = nuevaDuracion; 
+}
+void clsDataPeliculas::setGenero(const std::string& nuevoGenero) { 
+    std::strncpy(genero, nuevoGenero.c_str(), 29); genero[29] = '\0'; 
+}
+void clsDataPeliculas::setClasificacion(const std::string& nuevaClasificacion) { 
+    std::strncpy(clasificacion, nuevaClasificacion.c_str(), 3); clasificacion[3] = '\0';
+ }
+void clsDataPeliculas::setDirector(const std::string& nuevoDirector) { 
+    std::strncpy(director, nuevoDirector.c_str(), 49); director[49] = '\0'; 
+}
+/* void clsDataPeliculas::setFechaEstreno(const Fecha& fecha) { 
+ futura implementación 
+} */
+void clsDataPeliculas::setIdiomas(const std::string& nuevosIdiomas) { 
+    idiomas = nuevosIdiomas; 
+}
+void clsDataPeliculas::setFormatos(const std::string& nuevosFormatos) { 
+    formatos = nuevosFormatos; 
+}
+void clsDataPeliculas::setActiva(bool estado) { 
+    activa = estado; 
+}
---
+++ functions/peliculas/clsdataPeliculas.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <string>
+
+class clsDataPeliculas {
+private:
+    char id[11];           // AAAA-MM-XXXX
+    char nombre[50];
+    int duracion;          // en minutos
+    char genero[30];
+    char clasificacion[4]; // ATP, +16, +18
+    char director[50];
+    // Fecha estreno;      /* <-- Se agregará la clase Fecha más adelante */
+    std::string idiomas;   // Español, Ingles subtitulado, Original subtitulado
+    std::string formatos;  // 2D, 3D, 4D
+    bool activa;
+
+public:
+    clsDataPeliculas();
+
+    // Getters
+    std::string getId() const;
+    std::string getNombre() const;
+    int getDuracion() const;
+    std::string getGenero() const;
+    std::string getClasificacion() const;
+    std::string getDirector() const;
+    // Fecha getFechaEstreno() const; /* <-- futura implementación */
+    std::string getIdiomas() const;
+    std::string getFormatos() const;
+    bool estaActiva() const;
+
+    // Setters
+    void setId(const std::string& nuevoId);
+    void setNombre(const std::string& nuevoNombre);
+    void setDuracion(int nuevaDuracion);
+    void setGenero(const std::string& nuevoGenero);
+    void setClasificacion(const std::string& nuevaClasificacion);
+    void setDirector(const std::string& nuevoDirector);
+    // void setFechaEstreno(const Fecha& fecha); /* <-- futura implementación */
+    void setIdiomas(const std::string& nuevosIdiomas);
+    void setFormatos(const std::string& nuevosFormatos);
+    void setActiva(bool estado);
+};
